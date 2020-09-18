6f9157983a554bc770b4f89d17029d758aa5b470
Bowsil <uaekabali@gmail.ocm>
2020-09-18 12:48:23 +0400

    Updated

--- modules/redisconnectiontimeout.js
+++ modules/redisconnectiontimeout.js
@@ -8,7 +8,7 @@ module.exports = (jwtid, dbvalue, gettype, timeoutval, callback) => {
         { auth_pass: "qDwjAsMvQbWKO+w8ii+RZWWJDQI5aBIC4bAOkHnYnoY=", tls: { servername: "agentapp.redis.cache.windows.net" } });
 
     ///TODO : Thorw error in case of Connection Error 
-    //TODO : Execption handling 
+    //TODO : Execption handling  yes we need to do this because of redis 
     client.on("error", function (error) {
         console.error(error);
     });
