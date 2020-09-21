671d5a109d1107723f24a8fdf53e30f3ae3aa307
Bowsil <uaekabali@gmail.ocm>
2020-09-21 09:36:52 +0400

    Updated

--- modules/redisconnection.js
+++ modules/redisconnection.js
@@ -5,7 +5,9 @@ module.exports = (jwtid, dbvalue, gettype, callback) => {
     var redis = require("redis");
     ///TODO: Move this to ENV Variable
     var client = redis.createClient(6380, "agentapp.redis.cache.windows.net",
-        { auth_pass: "qDwjAsMvQbWKO+w8ii+RZWWJDQI5aBIC4bAOkHnYnoY=", tls: { servername: "agentapp.redis.cache.windows.net" } });
+        {
+            connectTimeout: 30000
+, auth_pass: "qDwjAsMvQbWKO+w8ii+RZWWJDQI5aBIC4bAOkHnYnoY=", tls: { servername: "agentapp.redis.cache.windows.net" } });
 
     ///TODO : Thorw error in case of Connection Error 
     //TODO : Execption handling 
--- modules/redisconnectiontimeout.js
+++ modules/redisconnectiontimeout.js
@@ -5,7 +5,7 @@ module.exports = (jwtid, dbvalue, gettype, timeoutval, callback) => {
     var redis = require("redis");
     ///TODO: Move this to ENV Variable
     var client = redis.createClient(6380, "agentapp.redis.cache.windows.net",
-        { auth_pass: "qDwjAsMvQbWKO+w8ii+RZWWJDQI5aBIC4bAOkHnYnoY=", tls: { servername: "agentapp.redis.cache.windows.net" } });
+        { connectTimeout: 30000, auth_pass: "qDwjAsMvQbWKO+w8ii+RZWWJDQI5aBIC4bAOkHnYnoY=", tls: { servername: "agentapp.redis.cache.windows.net" } });
 
     ///TODO : Thorw error in case of Connection Error 
     //TODO : Execption handling  yes we need to do this because of redis 
